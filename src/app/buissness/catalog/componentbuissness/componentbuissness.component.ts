dfcad289f0da620c4333353ccec477b4d1b83ffd
zahid492 <zahidcse09@gmail.com>
2018-02-03 16:17:58 +0600

    Componet Buissness works

--- src/app/buissness/catalog/componentbuissness/componentbuissness.component.ts
+++ src/app/buissness/catalog/componentbuissness/componentbuissness.component.ts
@@ -1,31 +1,15 @@
-import { Component, OnInit, OnDestroy} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import {trigger, transition, query, animate, style} from '@angular/animations';
-import {Subscription} from 'rxjs/Subscription';
 @Component({
   selector: 'app-buissness',
-  templateUrl: './componentbuissness.component.html',
-  styleUrls: ['./componentbuissness.component.css'],
-  animations: [
-    trigger('routerAnimations', [
-      transition('* => *', [
-        query(':enter', style({ height: 0, overflow: 'hidden' }), { optional: true }),
-        query(':leave', [
-          animate('500ms ease', style({ opacity: 0, transform: 'translateY(100px)' }))
-        ], { optional: true })
-      ])
-    ])
-  ]
+  templateUrl: './componentBuissness.component.html',
+  styleUrls: ['./componentBuissness.component.css']
 })
+export class ComponentBuissnessComponent implements OnInit {
 
-export class ComponentbuissnessComponent implements OnInit, OnDestroy {
-  subscription: Subscription;
   constructor() { }
-  name= 'hello';
+
   ngOnInit() {
-     this.name = 'hello';
   }
-  ngOnDestroy() {
-}
 
 }
